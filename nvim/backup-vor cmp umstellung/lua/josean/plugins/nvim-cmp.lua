ceb2ea20a595f95a9fb81a6c090433c87b19bc1d
ghaarm <>
2024-07-13 21:21:09 +0200

    - in der lazy.lua nvim-cmp explizit importiert, jetzt funktioniert es - next step init.lua in nvim hauptordner aufräumen

--- nvim/backup-vor cmp umstellung/lua/josean/plugins/nvim-cmp.lua
+++ nvim/backup-vor cmp umstellung/lua/josean/plugins/nvim-cmp.lua
@@ -1,87 +1,181 @@
 return {
   "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
+  event = { "InsertEnter", "CmdlineEnter" },
   dependencies = {
-    "hrsh7th/cmp-buffer", -- source for text in buffer
-    "hrsh7th/cmp-path", -- source for file system paths
-    "hrsh7th/cmp-cmdline", -- Command-line completions
-    "hrsh7th/cmp-nvim-lsp", -- LSP completions
-    {
-      "L3MON4D3/LuaSnip",
-      -- follow latest release.
-      version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-      -- install jsregexp (optional!).
-      build = "make install_jsregexp",
-    },
+    "hrsh7th/cmp-buffer",       -- source for text in buffer
+    "hrsh7th/cmp-path",         -- source for file system paths
+    "L3MON4D3/LuaSnip",         -- snippet engine
     "saadparwaiz1/cmp_luasnip", -- for autocompletion
-    "rafamadriz/friendly-snippets", -- useful snippets
-    "onsails/lspkind.nvim", -- vs-code like pictograms
-    "R-nvim/cmp-r", -- R language support for nvim-cmp
+    -- "rafamadriz/friendly-snippets", -- useful snippets
+    -- "onsails/lspkind.nvim", -- vs-code like pictograms
+    "hrsh7th/cmp-cmdline",
+    "petertriho/cmp-git",
+    "f3fora/cmp-spell",
+    "micangl/cmp-vimtex",
+    -- "aspeddro/cmp-pandoc.nvim",
   },
   config = function()
     local cmp = require("cmp")
 
     local luasnip = require("luasnip")
 
-    local lspkind = require("lspkind")
-
-    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-    require("luasnip.loaders.from_vscode").lazy_load()
-
-    -- verweis auf die keybindings.lua für die cmo mappings
-    local mappings = {}
-    for k, v in pairs(_G.cmp_mappings) do
-      mappings[k] = cmp.mapping[v[1]](unpack(v[2]))
-    end
+    local kind_icons = {
+      article = "󰧮",
+      book = "",
+      incollection = "󱓷",
+      Function = "󰊕",
+      Constructor = "",
+      Text = "󰦨",
+      Method = "",
+      Field = "󰅪",
+      Variable = "󱃮",
+      Class = "",
+      Interface = "",
+      Module = "",
+      Property = "",
+      Unit = "",
+      Value = "󰚯",
+      Enum = "",
+      Keyword = "",
+      Snippet = "",
+      Color = "󰌁",
+      -- Color = "",
+      File = "",
+      Reference = "",
+      Folder = "",
+      EnumMember = "",
+      -- spell = "",
+      -- EnumMember = "",
+      Constant = "󰀫",
+      Struct = "",
+      -- Struct = "",
+      Event = "",
+      Operator = "󰘧",
+      TypeParameter = "",
+    }
+    -- find more here: https://www.nerdfonts.com/cheat-sheet
 
     cmp.setup({
       completion = {
-        completeopt = "menu,menuone,preview,noselect",
+        completeopt = "menu,noselect",
+        -- completeopt = "menuone,preview,noinsert,noselect",
+        keyword_length = 1,
       },
       snippet = { -- configure how nvim-cmp interacts with snippet engine
         expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
       },
-      mapping = {
-        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<C-e>"] = cmp.mapping({
-          i = cmp.mapping.abort(),
-          c = cmp.mapping.close(),
-        }),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp.mapping.select_next_item(),
-        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-        ["<C-j>"] = cmp.mapping.select_next_item(),
-        ["<C-k>"] = cmp.mapping.select_prev_item(),
-      },
       mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expandable() then
+            luasnip.expand()
+          elseif luasnip.jumpable() then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Bestätigt die Auswahl mit Enter
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
       }),
+      -- formatting for autocompletion
+      formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+          vim_item.kind = string.format("%s", kind_icons[vim_item.kind]) -- Kind icons
+          vim_item.menu = ({
+            -- vimtex = (vim_item.menu ~= nil and vim_item.menu or "[VimTex]"),
+            -- vimtex = test_fn(vim_item.menu, entry.source.name),
+            vimtex = vim_item.menu,
+            luasnip = "[Snippet]",
+            nvim_lsp = "[LSP]",
+            buffer = "[Buffer]",
+            spell = "[Spell]",
+            -- orgmode = "[Org]",
+            -- latex_symbols = "[Symbols]",
+            cmdline = "[CMD]",
+            path = "[Path]",
+          })[entry.source.name]
+          return vim_item
+        end,
+      },
       -- sources for autocompletion
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
-        { name = "cmp_r" }, --für R https://github.com/R-nvim/cmp-r
         { name = "luasnip" }, -- snippets
-        { name = "buffer" }, -- text within current buffer
-        { name = "path" }, -- file system paths
-      }),
+        { name = "vimtex" },
+        -- { name = "orgmode" },
+        -- { name = "pandoc" },
+        -- { name = "omni" },
+        { name = "buffer",  keyword_length = 3 }, -- text within current buffer
+        {
+          name = "spell",
+          keyword_length = 4,
+          option = {
+            keep_all_entries = false,
+            enable_in_context = function()
+              return true
+            end
+          },
+        },
+        { name = "path" },
 
-      -- configure lspkind for vs-code like pictograms in completion menu
-      -- formatting = {
-      --   format = lspkind.cmp_format({
-      --     maxwidth = 50,
-      --     ellipsis_char = "...",
-      --   }),
-      -- },
+        { name = "cmp_r" },
+      }),
+      confirm_opts = {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+      },
+      view = {
+        entries = 'custom',
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+        -- completion = {
+        --   border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+        -- },
+        -- documentation = {
+        --   border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+        -- },
+      },
+      performance = {
+        trigger_debounce_time = 500,
+        throttle = 550,
+        fetching_timeout = 80,
+      },
+    })
+
+    -- `/` cmdline setup.
+    cmp.setup.cmdline('/', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' }
+      }
+    })
+
+    -- `:` cmdline setup.
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'path' },
+        { name = 'cmdline' }
+      }
     })
   end,
 }
