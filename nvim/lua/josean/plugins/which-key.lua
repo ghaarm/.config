84d0298d8b415ee0b123d40a008512511278b0a3
ghaarm <>
2024-08-25 22:44:56 +0200

    Add vim-eunuch, which-key with empty opts
    - back up copy of which key in deprecated
    - left opt empty and commented functions at the end
    - now no more warnings at nvim start up

--- nvim/lua/josean/plugins/which-key.lua
+++ nvim/lua/josean/plugins/which-key.lua
@@ -6,80 +6,54 @@ return {
     vim.o.timeoutlen = 500
   end,
   opts = {
-    setup = {
-      show_help = true,
-      plugins = {
-        presets = {
-          operators = false,    -- adds help for operators like d, y, ... and registers them for motion / text object completion
-          motions = false,      -- adds help for motions
-          text_objects = false, -- help for text objects triggered after entering an operator
-          windows = false,      -- default bindings on <c-w>
-          nav = false,          -- misc bindings to work with windows
-          z = false,            -- bindings for folds, spelling and others prefixed with z
-          g = false,            -- bindings for prefixed with g
-          marks = true,         -- shows a list of your marks on ' and `
-          registers = true,     -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-          spelling = {
-            enabled = false,    -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-            suggestions = 10,   -- how many suggestions should be shown in the list?
-          },
-          -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-          -- No actual key bindings are created
-        },
-      },
-      triggers = { "<leader>" }, -- or specify a list manually
-    },
-    defaults = {
-      buffer = nil,     -- Global mappings. Specify a buffer number for buffer local mappings
-      silent = true,    -- use `silent` when creating keymaps
-      noremap = true,   -- use `noremap` when creating keymaps
-      nowait = true,    -- use `nowait` when creating keymaps
-      prefix = "<leader>",
-      t = {
-        name = "TEMPLATES",
-        a = {
-          "<cmd>read ~/.config/nvim/templates/article-template.tex<CR>",
-          "article-template.tex",
-        },
-        b = {
-          "<cmd>read ~/.config/nvim/templates/footer-footnotes-bibliography.tex<CR>",
-          "footer-footnotes-bibliography.tex",
-        },
-        c = {
-          "<cmd>read ~/.config/nvim/templates/chapter.tex<CR>",
-          "chapter template tex",
-        },
-
-        r = {
-          "<cmd>read ~/.config/nvim/templates/book.tex<CR>",
-          "report template tex",
-        },
-        l = {
-          "<cmd>read ~/.config/nvim/templates/footer-footnotes.tex<CR>",
-          "footer-footnotes.tex",
-        },
-        m = {
-          "<cmd>read ~/.config/nvim/templates/mail.txt<CR>",
-          "mail.txt",
-        },
-        mm = {
-          "<cmd>read ~/.config/nvim/templates/markdown.md<CR>",
-          "markdown.md",
-        },
-        s = {
-          "<cmd>read ~/.config/nvim/templates/cheat-sheet.md<CR>",
-          "cheat-sheet.md",
-        },
-        u = {
-          "<cmd>read ~/.config/nvim/templates/übergabeprotokoll.tex<CR>",
-          "übergabeprotokoll.tex",
-        },
-      },
-    },
+    -- defaults = {
+    --   preset = "classic",
+    --   notify = true,
+    --   plugins = {
+    --     marks = true,     -- shows a list of your marks on ' and `
+    --     registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+    --     -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+    --     -- No actual key bindings are created
+    --     spelling = {
+    --       enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+    --       suggestions = 20, -- how many suggestions should be shown in the list?
+    --     },
+    --     presets = {
+    --       operators = false,    -- adds help for operators like d, y, ...
+    --       motions = false,      -- adds help for motions
+    --       text_objects = false, -- help for text objects triggered after entering an operator
+    --       windows = false,      -- default bindings on <c-w>
+    --       nav = false,          -- misc bindings to work with windows
+    --       z = false,            -- bindings for folds, spelling and others prefixed with z
+    --       g = false,            -- bindings for prefixed with g
+    --     },
+    --   },
+    -- },
   },
+  -- keys = {
+  --   {
+  --     "<leader>?",
+  --     function()
+  --       require("which-key").show({ global = false })
+  --     end,
+  --     desc = "Buffer Local Keymaps (which-key)",
+  --   },
+  -- },
   config = function(_, opts)
     local wk = require("which-key")
-    wk.setup(opts.setup)
-    wk.register(opts.defaults)
+    wk.add({
+      { "<leader>t",   group = "TEMPLATES",                                                        nowait = true,                              remap = false },
+      { "<leader>ta",  "<cmd>read ~/.config/nvim/templates/article-template.tex<CR>",              desc = "article-template.tex",              nowait = true, remap = false },
+      { "<leader>tb",  "<cmd>read ~/.config/nvim/templates/footer-footnotes-bibliography.tex<CR>", desc = "footer-footnotes-bibliography.tex", nowait = true, remap = false },
+      { "<leader>tc",  "<cmd>read ~/.config/nvim/templates/chapter.tex<CR>",                       desc = "chapter template tex",              nowait = true, remap = false },
+      { "<leader>tl",  "<cmd>read ~/.config/nvim/templates/footer-footnotes.tex<CR>",              desc = "footer-footnotes.tex",              nowait = true, remap = false },
+      { "<leader>tm",  "<cmd>read ~/.config/nvim/templates/mail.txt<CR>",                          desc = "mail.txt",                          nowait = true, remap = false },
+      { "<leader>tmm", "<cmd>read ~/.config/nvim/templates/markdown.md<CR>",                       desc = "markdown.md",                       nowait = true, remap = false },
+      { "<leader>tr",  "<cmd>read ~/.config/nvim/templates/book.tex<CR>",                          desc = "report template tex",               nowait = true, remap = false },
+      { "<leader>ts",  "<cmd>read ~/.config/nvim/templates/cheat-sheet.md<CR>",                    desc = "cheat-sheet.md",                    nowait = true, remap = false },
+      { "<leader>tu",  "<cmd>read ~/.config/nvim/templates/übergabeprotokoll.tex<CR>",             desc = "übergabeprotokoll.tex",             nowait = true, remap = false },
+    })
+    -- wk.setup(opts.setup)
+    -- wk.register(opts.defaults)
   end,
 }
