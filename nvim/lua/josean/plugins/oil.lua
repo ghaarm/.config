2fb38d9cc872806ced9e95773a62acfdc8fabd0d
ghaarm <>
2024-09-18 11:30:59 +0200

    Add flash, oil gx for nvim
    - gx needed because of oil for links

---
+++ nvim/lua/josean/plugins/oil.lua
@@ -0,0 +1,27 @@
+-- Andrew Courter https://www.youtube.com/watch?v=q1QhV-24DNA
+
+return {
+  'stevearc/oil.nvim',
+  opts = {},
+  -- Optional dependencies
+  -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  config = function()
+    require("oil").setup({
+      default_file_explorer = true,
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true, -- Use `show_hidden` instead of `how_hidden`
+        natural_order = true,
+        is_always_hidden = function(name, _)
+          return name == '..' or name == '.git'
+        end,
+      },
+      win_options = {
+        wrap = true,
+      }
+    })
+  end,
+  vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+}
