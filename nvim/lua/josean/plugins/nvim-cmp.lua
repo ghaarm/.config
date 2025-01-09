320b25fa3f4584a5e69c09b677145feab4e7b52b
g <g@unknownda1a34b38f98.localdomain>
2025-01-09 23:19:06 +0100

    Nvim cmp enter doesnt choose unselected word, yazi start

--- nvim/lua/josean/plugins/nvim-cmp.lua
+++ nvim/lua/josean/plugins/nvim-cmp.lua
@@ -89,7 +89,19 @@ return {
           end
         end, { 'i', 's' }),
 
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Bestätigt die Auswahl mit Enter
+        -- ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Bestätigt die Auswahl mit Enter
+        -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#safely-select-entries-with-cr damit bei enter nicht etwas ausgewählt wird auch wenn nicht markiert ist
+        ["<CR>"] = cmp.mapping({
+       i = function(fallback)
+         if cmp.visible() and cmp.get_active_entry() then
+           cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+         else
+           fallback()
+         end
+       end,
+       s = cmp.mapping.confirm({ select = true }),
+       c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+     }),
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
       }),
