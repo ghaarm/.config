d7476f61297e79556e4c29b71e7483eee85bf978
ghaarm <ghaarm@tutanota.com>
2026-03-17 08:54:21 +0100

    Add liveserver starst stop and harpoon pathes

--- nvim/lua/plugins/live-server.lua
+++ nvim/lua/plugins/live-server.lua
@@ -1,8 +1,11 @@
 -- https://github.com/barrett-ruth/live-server.nvim
-
 return {
   "barrett-ruth/live-server.nvim",
-  build = 'pnpm add -g live-server',
-  cmd = { 'LiveServerStart', 'LiveServerStop' },
-  config = true
-}
+  build = "pnpm add -g live-server",
+  cmd = { "LiveServerStart", "LiveServerStop" },
+  keys = {
+    { "<leader>ls", "<cmd>LiveServerStart<CR>", desc = "Live Server Start" },
+    { "<leader>lc", "<cmd>LiveServerStop<CR>", desc = "Live Server Stop" },
+  },
+  config = true,
+}-
