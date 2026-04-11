ae94a19531a2d51241f0c1bb6aa5895ebeb7824e
ghaarm <ghaarm@tutanota.com>
2026-04-11 22:30:56 +0200

    Improve winbar not to show same name of active buffer in splits

--- nvim/lua/core/functions/winbar.lua
+++ nvim/lua/core/functions/winbar.lua
@@ -1,7 +1,7 @@
--- -- tj devries https://www.youtube.com/watch?v=LKW_SUucO-k&list=PLMLyZFWQbxIzctnJSWserv5InbRwHm-oW&index=24
--- --
+-- tj devries https://www.youtube.com/watch?v=LKW_SUucO-k&list=PLMLyZFWQbxIzctnJSWserv5InbRwHm-oW&index=24
+--
 local M = {}
-local dir_levels = 1 -- anzahl der directories
+local dir_levels = 1
 
 local function tail_path(path, n_dirs)
   if path == "" then
@@ -25,24 +25,25 @@ local function tail_path(path, n_dirs)
 end
 
 function M.render()
-  local bt = vim.bo.buftype
-  local ft = vim.bo.filetype
-  local name = vim.api.nvim_buf_get_name(0)
+  local winid = vim.g.statusline_winid or vim.api.nvim_get_current_win()
+  local bufnr = vim.api.nvim_win_get_buf(winid)
+
+  local ft = vim.bo[bufnr].filetype
+  local name = vim.api.nvim_buf_get_name(bufnr)
+  local modified = vim.bo[bufnr].modified and "[+]" or ""
 
   -- Alpha-Startseite ausblenden
-  -- if ft == "alpha" then
+  if ft == "alpha" or ft == "oil" then
+    return ""
+  end
+
+  -- unbenannten/leeren Buffer ausblenden
+  -- if name == "" then
   --   return ""
   -- end
-  if ft == "alpha" or ft == "oil" or name == "" then
-    return ""
-  end
-  -- unbenannten/leeren Buffer ausblenden
-  if name == "" then
-    return ""
-  end
-  local path = vim.fn.expand("%:p")
-  local shown = tail_path(path, dir_levels)
-  return "%=%m " .. shown
+
+  local shown = tail_path(name, dir_levels)
+  return "%=" .. modified .. " " .. shown
 end
 
 function M.setup()
