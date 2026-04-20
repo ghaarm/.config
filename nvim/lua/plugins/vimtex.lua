f6e8cb9e65b63c5217a3116de6546b7fa89e1963
ghaarm <ghaarm@tutanota.com>
2026-04-20 21:25:16 +0200

    Change vimtex so dass xelatex wirklich primär genommen wird
    - um andere Schriften zeigen zu können

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,75 +1,4 @@
 -- https://github.com/lervag/vimtex
---
--- vim.g.maplocalleader = ","
---
--- return {
---   "lervag/vimtex",
---   -- dependencies = {
---   --   "xuhdev/vim-latex-live-preview",
---   -- },
---   config = function()
---     -- Lokale Funktion zum Schreiben des Servernamens
---     -- https://jdhao.github.io/2021/02/20/inverse_search_setup_neovim_vimtex/
---     -- der eigentliche code der Homepage hatte nicht funktioniert, aber dieser hier
---     local function write_server_name()
---       local nvim_server_file = (vim.fn.has("win32") == 1 and os.getenv("TEMP") or "/tmp") .. "/vimtexserver.txt"
---       local servername = vim.v.servername
---       local file = io.open(nvim_server_file, "w")
---       file:write(servername)
---       file:close()
---     end
---
---     -- Auto-Befehle für VimTeX
---     vim.api.nvim_create_augroup("vimtex_common", { clear = true })
---     vim.api.nvim_create_autocmd("FileType", {
---       pattern = "tex",
---       callback = write_server_name,
---       group = "vimtex_common",
---     })
---
---     -- Vimtex Konfiguration
---
---     -- vim.g.tex_flavor = "latex"
---     vim.g.vimtex_compiler_method = "latexmk"
---     vim.g.vimtex_view_method = "skim"
---
---     vim.g.vimtex_view_skim_sync = 1     -- Value 1 allows forward search after every successful compilation
---     vim.g.vimtex_view_skim_activate = 1 --# Value 1 allows change focus to skim after command `:VimtexView` is given
---
---     -- Skim Konfiguration für Vimtex
---     -- vim.g.vimtex_view_skim = {
---     --   executable = 'open',
---     --   args = {'-a', 'Skim'}
---     -- }
---
---     -- Kürzel für Vimtex-Funktionen über den lokalen Leader
---     vim.api.nvim_set_keymap("n", "<Localleader>v", ":VimtexView<CR>", { noremap = true, silent = true })
---
---     -- latexmk configuration to remove auxiliary files except for .log
---     vim.g.vimtex_compiler_latexmk = {
---       build_dir = "",
---       callback = 1,
---       continuous = 1,
---       executable = "latexmk",
---       options = {
---         "-pdf",
---         "-shell-escape",
---         "-verbose",
---         "-file-line-error",
---         "-interaction=nonstopmode",
---         "-synctex=1",
---       },
---     }
---
---     -- Zusätzliche Konfigurationen damit vimtex immer das Verzeichnis der tex datei als arbeitsverzeichnis benutzt
---     vim.g.vimtex_syntax_enabled = 1
---     vim.g.vimtex_quickfix_open_on_warning = 0
---
---     -- Mapping für die Bereinigung der Hilfsdateien im richtigen Verzeichnis
---     -- vim.api.nvim_set_keymap('n', '<Localleader>c', ':lua CleanLatexFiles()<CRG>', { noremap = true, silent = true })
---   end,
--- }
---
 
 vim.g.maplocalleader = ","
 
@@ -132,33 +61,53 @@ return {
     --
     --
     -- latexmk configuration to remove auxiliary files except for .log
+    -- vim.g.vimtex_compiler_latexmk = {
+    --   build_dir = "",
+    --   callback = 1,
+    --   continuous = 1,
+    --   executable = "latexmk",
+    --   -- options = {
+    --   --   "-pdfxe", -- <- wichtig: PDF-Modus mit xelatex
+    --   --   "-shell-escape",
+    --   --   "-verbose",
+    --   --   "-file-line-error",
+    --   --   "-interaction=nonstopmode",
+    --   --   "-synctex=1",
+    --   -- },
+    --   options = {
+    --     -- "-pdf",
+    --     "-xelatex", -- damit ich mit Iosevka kompilieren kann
+    --     "-shell-escape",
+    --     "-verbose",
+    --     "-file-line-error",
+    --     "-interaction=nonstopmode",
+    --     "-synctex=1",
+    --   },
+    --   -- Hier den `aux_dir` einfügen, um die Hilfsdateien in einem speziellen Verzeichnis abzulegen
+    --   aux_dir = "auxiliary_files",
+    -- }
+    vim.g.vimtex_compiler_method = "latexmk"
+
+    vim.g.vimtex_compiler_latexmk_engines = {
+      _ = "-xelatex",
+      xelatex = "-xelatex",
+      pdflatex = "-pdf",
+      lualatex = "-lualatex",
+    }
+
     vim.g.vimtex_compiler_latexmk = {
-      build_dir = "",
       callback = 1,
       continuous = 1,
       executable = "latexmk",
-      -- options = {
-      --   "-pdfxe", -- <- wichtig: PDF-Modus mit xelatex
-      --   "-shell-escape",
-      --   "-verbose",
-      --   "-file-line-error",
-      --   "-interaction=nonstopmode",
-      --   "-synctex=1",
-      -- },
       options = {
-        -- "-pdf",
-        "-xelatex", -- damit ich mit Iosevka kompilieren kann
         "-shell-escape",
         "-verbose",
         "-file-line-error",
         "-interaction=nonstopmode",
         "-synctex=1",
       },
-      -- Hier den `aux_dir` einfügen, um die Hilfsdateien in einem speziellen Verzeichnis abzulegen
       aux_dir = "auxiliary_files",
-    }
-
-    -- Zusätzliche Konfigurationen damit vimtex immer das Verzeichnis der tex datei als arbeitsverzeichnis benutzt
+    } -- Zusätzliche Konfigurationen damit vimtex immer das Verzeichnis der tex datei als arbeitsverzeichnis benutzt
     vim.g.vimtex_syntax_enabled = 1
     vim.g.vimtex_quickfix_open_on_warning = 0
 
