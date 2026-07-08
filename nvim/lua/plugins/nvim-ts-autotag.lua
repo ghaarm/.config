993b31f81bac202e5dbe6459a2ab1d70e4342caf
ghaarm <ghaarm@tutanota.com>
2026-07-08 12:31:41 +0200

    change nvim-ts-autotag and treesitter wg config change nvim 0.12

--- nvim/lua/plugins/nvim-ts-autotag.lua
+++ nvim/lua/plugins/nvim-ts-autotag.lua
@@ -1,25 +1,47 @@
+-- return {
+--   "windwp/nvim-ts-autotag",
+--   event = "VeryLazy",
+--   dependencies = { "nvim-treesitter/nvim-treesitter" },
+--   config = function()
+--     require("nvim-treesitter.configs").setup({
+--       autotag = {
+--         enable = true,
+--         filetypes = {
+--           "html",
+--           "xml",
+--           "javascript",
+--           "typescript",
+--           "javascriptreact",
+--           "typescriptreact",
+--           "svelte",
+--           "vue",
+--           -- und hier deine:
+--           "r",
+--           "rmd",
+--           "rmarkdown",
+--         },
+--       },
+--     })
+--   end,
+-- }
+--
+-- neu wg. nvim 0.12
+--
 return {
   "windwp/nvim-ts-autotag",
-  event = "VeryLazy",
+  event = { "BufReadPre", "BufNewFile" },
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   config = function()
-    require("nvim-treesitter.configs").setup({
-      autotag = {
-        enable = true,
-        filetypes = {
-          "html",
-          "xml",
-          "javascript",
-          "typescript",
-          "javascriptreact",
-          "typescriptreact",
-          "svelte",
-          "vue",
-          -- und hier deine:
-          "r",
-          "rmd",
-          "rmarkdown",
-        },
+    require("nvim-ts-autotag").setup({
+      opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = false,
+      },
+      aliases = {
+        r = "html",
+        rmd = "html",
+        rmarkdown = "html",
       },
     })
   end,
