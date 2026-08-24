c673ba3bc3d8e3f3aad61cf1e58767fd669b3252
ghaarm <ghaarm@tutanota.com>
2026-08-24 23:01:18 +0200

    add function tex to docx, markdownpreview lu

---
+++ nvim/lua/core/functions/tex-to-docx.lua
@@ -0,0 +1,70 @@
+local M = {}
+
+function M.tex_to_docx()
+  local tex = vim.api.nvim_buf_get_name(0)
+
+  if tex == "" or vim.bo.filetype ~= "tex" then
+    vim.notify("Keine LaTeX-Datei geöffnet", vim.log.levels.ERROR)
+
+    return
+  end
+
+  local dir = vim.fs.dirname(tex)
+
+  local stem = vim.fn.fnamemodify(tex, ":t:r")
+
+  local output = dir .. "/" .. stem .. ".docx"
+
+  local bib = vim.fn.expand("~/Library/texmf/bibtex/bib/Zotero.bib")
+
+  if vim.fn.filereadable(bib) == 0 then
+    vim.notify("Bib-Datei nicht gefunden: " .. bib, vim.log.levels.ERROR)
+
+    return
+  end
+
+  local args = {
+
+    "pandoc",
+
+    tex,
+
+    "--from=latex",
+
+    "--to=docx",
+
+    "--citeproc",
+
+    "--bibliography=" .. bib,
+
+    "--resource-path=" .. dir,
+
+    "-o",
+
+    output,
+  }
+
+  vim.notify("Erstelle DOCX …")
+
+  vim.system(args, {
+
+    cwd = dir,
+
+    text = true,
+  }, function(result)
+    vim.schedule(function()
+      if result.code == 0 then
+        vim.notify("DOCX erstellt: " .. output)
+      else
+        vim.notify("Pandoc-Fehler:\n" .. (result.stderr or ""), vim.log.levels.ERROR)
+      end
+    end)
+  end)
+end
+
+vim.keymap.set("n", "<localleader>ld", M.tex_to_docx, {
+
+  desc = "LaTeX → DOCX",
+})
+
+return M
