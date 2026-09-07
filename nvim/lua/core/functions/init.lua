4d5de809195b0dc3758970d06600e695c5ac2283
ghaarm <ghaarm@tutanota.com>
2026-09-07 10:11:07 +0200

    Add yazi function for oil und buffer open

--- nvim/lua/core/functions/init.lua
+++ nvim/lua/core/functions/init.lua
@@ -1,4 +1,5 @@
 require("core.functions.open-in-finder")
+require("core.functions.open-in-yazi")
 require("core.functions.word-templates")
 require("core.functions.tex-to-docx")
 require("core.functions.winbar").setup()
