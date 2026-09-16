73504b764f97ac8d195723b2afb30b65ebb042f9
ghaarm <ghaarm@tutanota.com>
2026-09-16 12:02:30 +0200

    Patch vimtex final normal compilation stop

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -50,6 +50,10 @@ return {
     -- Kürzel für Vimtex-Funktionen über den lokalen Leader
     vim.api.nvim_set_keymap("n", "<Localleader>lu", ":VimtexCompile<CR>", { noremap = true, silent = true })
     vim.keymap.set("n", "<Localleader>lf", function()
+      -- Laufenden VimTeX-/latexmk-Continuous-Build stoppen
+      vim.cmd("VimtexStop")
+
+      -- Datei speichern
       vim.cmd("write")
 
       local texfile = vim.fn.expand("%:t")
